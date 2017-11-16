89c212884e6454e2b2a922a689ac9dc2818c8a54
lfl7824833 <lfl7824833@vip.qq.com>
2017-11-16 16:43:20 +0000

    fangling reverse integer

--- Math/Fangling_reverseInteger.py
+++ Math/Fangling_reverseInteger.py
@@ -1,7 +1,36 @@
 class Solution:
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        
+	def reverse(self, x):
+
+		if x > 0:
+
+			x = list(map(int,str(x)))
+			x = x[::-1]
+			results=0
+			for i in range(len(x)):
+				results += x[i] * (10**(len(x)-i-1))
+
+			if results > 2147483647:
+				return 0
+
+			return results
+		elif x < 0:
+			x = x*(-1)
+			x = list(map(int,str(x)))
+			x = x[::-1]
+			results=0
+			for i in range(len(x)):
+				results += x[i] * (10**(len(x)-i-1))
+			
+			if results < -2147483647:
+				return 0
+
+			return -results
+		else:
+			return 0
+
+
+
+
+x=120
+hi=Solution()
+hi.reverse(x)
