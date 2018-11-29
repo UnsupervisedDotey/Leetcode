770b151ea2e44f95424d106bcd8162c44067766b
FunLing <17073304+lfl7824833@users.noreply.github.com>
2018-11-29 19:03:29 +0800

    手写排序？不存在的？

---
+++ Array/215_findKthLargest.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return sorted(nums)[-k]
+
+
+sol = Solution()
+print(sol.findKthLargest([1,2,3], 2))
