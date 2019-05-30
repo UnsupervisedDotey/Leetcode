82388a09003913319c6f421444365feaf69c4f64
lfl7824833 <lfl7824833@vip.qq.com>
2019-05-30 15:20:37 +0800

    stack 682

---
+++ Stack/682_calPoints.py
@@ -0,0 +1,10 @@
+class Solution:
+    def calPoints(self, ops) -> int:
+
+        return 1
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    input_list = ["5", "2", "C", "D", "+"]
+    print(sol.calPoints(ops=input_list))
