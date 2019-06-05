29a2a1512a330197c259c25c9b5d9e406661cab6
lfl7824833 <lfl7824833@vip.qq.com>
2019-06-05 15:38:52 +0800

    执行用时 : 88 ms, 在Baseball Game的Python3提交中击败了11.80% 的用户 内存消耗 : 13.1 MB, 在Baseball Game的Python3提交中击败了74.63% 的用户

--- Stack/682_calPoints.py
+++ Stack/682_calPoints.py
@@ -48,5 +48,5 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    input_list = ["5","2","C","D","+"]
+    input_list = ["5", "2", "C", "D", "+"]
     print(sol.calPoints(ops=input_list))
