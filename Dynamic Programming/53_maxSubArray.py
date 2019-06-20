8888fef1c7169c071ca07806e16ade6ed0b1c8b0
lfl7824833 <lfl7824833@vip.qq.com>
2019-06-20 17:48:59 +0800

    执行用时 :116 ms, 在所有 Python3 提交中击败了7.34%的用户 内存消耗 :13.4 MB, 在所有 Python3提交中击败了96.67%的用户

---
+++ Dynamic Programming/53_maxSubArray.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        """
+
+        :param nums: input num list
+        :return: max sub array list
+        """
+
+        max_sum = 0
+        ans = 0
+
+        for n in nums:
+
+            if max_sum+n <= 0:
+                max_sum = n
+            else:
+                max_sum = max([max_sum+n, n])
+
+            ans = max([max_sum, ans])
+            # print(n, max_sum, ans)
+        if len([each for each in nums if each < 0]) == len(nums):
+            ans = max(nums)
+        return ans
+
+
+if __name__ == "__main__":
+    input_list = [-1]
+    sol = Solution()
+    print(sol.maxSubArray(nums=input_list))
