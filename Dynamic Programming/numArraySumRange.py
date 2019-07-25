a284a3a1542af81b33ab4cc9e07828941c88f4b4
lfl7824833 <lfl7824833@foxmail.com>
2019-07-25 08:51:18 +0800

    执行用时 :816 ms, 在所有 Python3 交中击败了40.29%的用户 内存消耗 :17.4 MB, 在所有 Python3 提交中击败了5.54%的用户

---
+++ Dynamic Programming/numArraySumRange.py
@@ -0,0 +1,17 @@
+class NumArray:
+
+    def __init__(self, nums):
+        self.sums = [0] + [sum(nums[0:l]) for l in range(1, len(nums)+1)]
+        # print(self.sums)
+
+    def sumRange(self, i: int, j: int) -> int:
+        # print(self.sums[j+1], self.sums[i])
+        return self.sums[j+1]-self.sums[i]
+
+
+if __name__ == "__main__":
+    nums = [-2, 0, 3, -5, 2, -1]
+    i, j = 2, 5
+    obj = NumArray(nums)
+    param_1 = obj.sumRange(i, j)
+    print(param_1)
