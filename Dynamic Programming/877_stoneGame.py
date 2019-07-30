4126633395e9beb94a0cc77794a8c149345841cc
lfl7824833 <lfl7824833@foxmail.com>
2019-07-30 11:27:04 +0800

    没做出来

--- Dynamic Programming/877_stoneGame.py
+++ Dynamic Programming/877_stoneGame.py
@@ -1,10 +1,37 @@
 class Solution:
     def stoneGame(self, piles) -> bool:
-        res = True
+        """
+        dp怎么还和DAG有向无环图搭上关系了
+
+        :param piles:
+        :return:
+        """
+        l = len(piles)
+        alex = []
+        lee = []
+        for i in range(int(l/2)):
+
+            pair = sorted([piles[0], piles[-1]], reverse=True)[0]
+            alex.append(pair)    # 大的
+            del piles[piles.index(pair)]
+            print(piles)
+
+            pair = sorted([piles[0], piles[-1]], reverse=True)[0]
+            lee.append(pair)    # 小的
+            del piles[piles.index(pair)]
+            print(piles)
+
+        print(alex, lee)
+
+        if sum(alex) > sum(lee):
+            res = True
+        else:
+            res = False
+
         return res
 
 
 if __name__ == "__main__":
     sol = Solution()
-    list_ = [5, 3, 4, 5]
+    list_ = [3,7,2,3]
     print(sol.stoneGame(piles=list_))
