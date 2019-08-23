4d1644b351a416b87264e87549e35f22334f72fe
lfl7824833 <lfl7824833@vip.qq.com>
2019-08-23 11:18:19 +0800

    执行用时 :284 ms, 在所有 MySQL 提交中击败了84.48%的用户

---
+++ SQL/626_UNION_exchange-seats.sql
@@ -0,0 +1,37 @@
+# 我
+SELECT id+1 id, student
+  FROM seat
+  WHERE id % 2 = 1 AND id <= (SELECT MAX(id) - MAX(id) % 2 FROM seat)
+UNION
+  SELECT id-1 id, student
+  FROM seat
+  WHERE id % 2 = 0 AND id <= (SELECT MAX(id) - MAX(id) % 2 FROM seat)
+UNION
+  SELECT id, student
+  FROM seat
+  WHERE id = (SELECT CASE WHEN MAX(id) % 2 = 1 THEN MAX(id) ELSE 0 END AS last
+              FROM seat)
+ORDER BY id
+
+
+
+
+# top1
+# ？？
+# 所以有 CASE WHEN .c1. THEN .do1. ELSE .do2. END AS new_c1 等价于 IF(c1，do1，do2)
+
+SELECT IF(id%2<>0,
+          IF(id<>(SELECT MAX(id) FROM seat), id+1, id),
+          id-1) id, student
+FROM seat ORDER BY id;
+
+
+# top2 这个好理解
+
+SELECT CASE
+		WHEN s.id % 2 = 1 AND s.id = (SELECT max(s2.id) FROM seat s2) then s.id
+		when s.id % 2 = 1 then s.id+1
+		when s.id % 2 = 0 then s.id-1
+		END as id, student
+from seat s
+ORDER BY id
