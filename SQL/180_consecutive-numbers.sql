87f9221fe36b7cc1be803fad172eee52d30e07c3
lfl7824833 <lfl7824833@vip.qq.com>
2019-08-24 21:58:55 +0800

    执行用时 :989 ms, 在所有 MySQL 提交中击败了18.68%的用户

--- SQL/180_consecutive-numbers.sql
+++ SQL/180_consecutive-numbers.sql
@@ -18,3 +18,23 @@ FROM
     On target_t0.SRank=cur_num_t1.Rank) result
 GROUP BY ConsecutiveNums
 HAVING ConsecutiveNums IS NOT NULL
+
+
+#leetcode
+SELECT *
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+
+
+
+# top 1
+select distinct Num as "ConsecutiveNums"
+from Logs
+where (Id + 1, Num) in (select * from Logs) and (Id + 2, Num) in (select * from Logs)
