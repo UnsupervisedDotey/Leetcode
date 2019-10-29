1c2d322847d705a888074d782f884406759a2fd1
lfl7824833 <lfl7824833@vip.qq.com>
2019-10-29 19:00:14 +0800

    执行用时 : 52 ms , 在所有 python3 提交中击败了 55.11% 的用户 内存消耗 : 13.9 MB , 在所有 python3 提交中击败了 100.00% 的用户

---
+++ 哈希/1027_unique-number-of-occurrences.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def uniqueOccurrences(self, arr) -> bool:
+        counts = Counter(arr).values()
+
+        return len(counts) == len(set(counts))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    arr = [1,2,2,1,1,3]
+    print(sol.uniqueOccurrences(arr=arr))
