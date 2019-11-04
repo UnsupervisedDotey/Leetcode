6fad4e1be7c158db26173b69453161576bbd190a
lfl7824833 <lfl7824833@vip.qq.com>
2019-11-04 20:06:26 +0800

    tree try

--- 二叉树/965 _ 单值二叉树.py
+++ 二叉树/965 _ 单值二叉树.py
@@ -25,10 +25,10 @@ def gen_tree(values: list) -> Union[TreeNode, None]:
     while 1:
         head = d.popleft()
         try:
-            head.l_node = TreeNode(next(iter_value))
-            d.append(head.l_node)
-            head.r_node = TreeNode(next(iter_value))
-            d.append(head.r_node)
+            head.left = TreeNode(next(iter_value))
+            d.append(head.left)
+            head.right = TreeNode(next(iter_value))
+            d.append(head.right)
         except StopIteration:
             break
     return root
@@ -37,35 +37,34 @@ def gen_tree(values: list) -> Union[TreeNode, None]:
 def pre_traverse_tree(node: TreeNode):
     if node is None:
         return
-    yield node.value
-    yield from pre_traverse_tree(node.l_node)
-    yield from pre_traverse_tree(node.r_node)
+    yield node.val
+    yield from pre_traverse_tree(node.left)
+    yield from pre_traverse_tree(node.right)
 
 
 def in_traverse_tree(node: TreeNode):
     if node is None:
         return
-    yield from pre_traverse_tree(node.l_node)
-    yield node.value
-    yield from pre_traverse_tree(node.r_node)
+    yield from pre_traverse_tree(node.left)
+    yield node.val
+    yield from pre_traverse_tree(node.right)
 
 
 def post_traverse_tree(node: TreeNode):
     if node is None:
         return
-    yield from pre_traverse_tree(node.l_node)
-    yield from pre_traverse_tree(node.r_node)
-    yield node.value
+    yield from pre_traverse_tree(node.left)
+    yield from pre_traverse_tree(node.right)
+    yield node.val
 
 
 
 if __name__ == "__main__":
-
-    tree = gen_tree(list(range(10)))
+    tree_value = [1,1,1,1,1,None,1]
+    tree = gen_tree(tree_value)
     print(list(pre_traverse_tree(tree)))
     print(list(in_traverse_tree(tree)))
     print(list(post_traverse_tree(tree)))
 
-    tree_value = [1,1,1,1,1,None,1]
-    for i in range(len(tree_value)):
-        c1 = add_node_in_order(elements)
+    
+
