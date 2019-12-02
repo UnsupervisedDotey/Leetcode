f7151af81877ad32afb93c95b43347d3235908cf
lfl7824833 <lfl7824833@vip.qq.com>
2019-12-02 17:55:38 +0800

    error

---
+++ Array/766_toeplitz-matrix.py
@@ -0,0 +1,30 @@
+import operator
+
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        if len(matrix) == 1 or len(matrix[0]) == 1:
+            return True
+
+        res = 1
+        for i in range(1, len(matrix)):
+            # print(matrix[i][1:], matrix[i - 1][:-1])
+            if operator.eq(matrix[i][1:], matrix[i - 1][:-1]):
+                res *= 1
+            else:
+                res *= 0
+
+        if len(matrix[0]) == 2:
+            res *= matrix[0][0] == matrix[-1][-1]
+
+        return res == 1
+
+
+if __name__ == "__main__":
+    matrix = [[97, 97],
+            [80, 97],
+            [10, 80]
+            ]
+
+    sol = Solution()
+    print(sol.isToeplitzMatrix(matrix=matrix))
