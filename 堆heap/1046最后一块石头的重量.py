25ecf23920c52fbb24a7a9997e3a2f067b595fb7
lfl7824833 <lfl7824833@vip.qq.com>
2019-12-18 13:29:54 +0800

    11

---
+++ 堆heap/1046最后一块石头的重量.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+
+        return 1
+
+
+if __name__ == "__main__":
+    pass
