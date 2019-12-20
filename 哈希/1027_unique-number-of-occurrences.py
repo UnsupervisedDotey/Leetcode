de09b243aede935ba4d7be7f15ef4cc184a7b96b
lfl7824833 <lfl7824833@vip.qq.com>
2019-12-20 16:19:33 +0800

    11

--- 哈希/1027_unique-number-of-occurrences.py
+++ 哈希/1027_unique-number-of-occurrences.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 class Solution:
     def uniqueOccurrences(self, arr) -> bool:
         counts = Counter(arr).values()
@@ -9,5 +10,5 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    arr = [1,2,2,1,1,3]
+    arr = [1, 2, 2, 1, 1, 3]
     print(sol.uniqueOccurrences(arr=arr))
