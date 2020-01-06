1f89d0f3dcd00df5abd2850177821f33de2a6960
lfl7824833 <lfl7824833@vip.qq.com>
2020-01-06 15:02:56 +0800

    11

--- String/5_longestPalindrome.py
+++ String/5_longestPalindrome.py
@@ -1,15 +1,15 @@
 class Solution(object):
     def __init__(self):
         self.maxlen = 1
-        self.index =  0
+        self.index = 0
 
     def extendPalindrome(self, start, end, s, lens):
-        while start >=0 and end < lens and s[start] == s[end]:
-            start -=1
+        while start >= 0 and end < lens and s[start] == s[end]:
+            start -= 1
             end += 1
         if self.maxlen < end - start - 1:
             self.maxlen = end - start - 1
-            self.index = start +1
+            self.index = start + 1
 
     def longestPalindrome(self, s):
         """
@@ -17,14 +17,14 @@ class Solution(object):
         :rtype: str
         """
         lens = len(s)
-        if lens < 2 :
+        if lens < 2:
             return s
-        for i in range(lens-1):
+        for i in range(lens - 1):
             # if i*2 +1 > self.maxlen and (lens - i - 1)*2 + 1 > self.maxlen:
-                self.extendPalindrome(i, i, s, lens)
+            self.extendPalindrome(i, i, s, lens)
             # if i*2 +2> self.maxlen and (lens - i - 1)*2 + 2 > self.maxlen:
-                self.extendPalindrome(i, i+1, s, lens)
-        return s[self.index : self.index + self.maxlen]
+            self.extendPalindrome(i, i + 1, s, lens)
+        return s[self.index: self.index + self.maxlen]
 
 
 sol = Solution()
